aff74b6e1f09fb7b6876c8f3bc08102a6cb3f695
MarcoElz <marco.eelizondo@gmail.com>
2020-03-31 16:40:15 -0600

    Simple Grid Generator

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] backgroundPrefabs;
+    GameObject[][] backgroundGrid;
+
+    private void Start()
+    {
+        GenerateGrid(5,5);
+    }
+
+    public void GenerateGrid(int x, int y)
+    {
+        //Inicializamos la matriz del escenario
+        backgroundGrid = new GameObject[x][];
+        for(int i = 0; i < x; i++)
+        {
+            backgroundGrid[i] = new GameObject[y];
+        }
+
+        //Instanciar el background
+        for(int i = 0; i < backgroundGrid.Length; i++)
+        {
+            for (int j = 0; j < backgroundGrid[i].Length; j++)
+            {
+                int random = Random.Range(0, backgroundPrefabs.Length);
+                Vector2 position = new Vector2(i, j);
+                backgroundGrid[i][j] = Instantiate(backgroundPrefabs[random], position, Quaternion.identity);
+            }
+        }
+
+
+    }
+
+}
